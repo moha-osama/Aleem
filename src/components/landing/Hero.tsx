acddd4e736f8a3cb9a298177a9a9b7c72d8d0131
moha-osama <zmuhamedosama@gmail.com>
2026-03-08 16:52:25 +0200

    fixed the image size

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,5 +1,5 @@
 import heroSvg from "/hero-svg.png";
-import heroBg from "/hero-img.png";
+import heroBg from "/hero-img.jpeg";
 import heroVideo from "/hero-bg-video.mp4";
 import { motion } from "framer-motion";
 import { useState } from "react";
